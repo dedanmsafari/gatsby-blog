3669885cc8cd8b4c17526c0ae97bbee6528a7fc4
dedanmsafari <dedan.msafari@gmail.com>
2022-09-29 15:26:41 +0300

    created gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -6,4 +8,45 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  return graphql(`
+    query MyQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result =>
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  )
 }
